f24a2b4cce8fa696ddbbd708f9b6a127ed91dc0c
Liam <liam.sch@gmail.com>
2016-12-05 20:01:04 +0100

    uploading everything

---
+++ convnet.py
@@ -0,0 +1,319 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.contrib.layers import regularizers
+
+
+class ConvNet(object):
+    """
+   This class implements a convolutional neural network in TensorFlow.
+   It incorporates a certain graph model to be trained and to be used
+   in inference.
+    """
+
+    def __init__(self, n_classes = 10, is_training=True, dropout_rate=0.):
+        """
+        Constructor for an ConvNet object. Default values should be used as hints for
+        the usage of each parameter.
+        Args:
+          n_classes: int, number of classes of the classification problem.
+                          This number is required in order to specify the
+                          output dimensions of the ConvNet.
+        """
+        self.n_classes = n_classes
+        self.is_training = is_training
+        self.dropout_rate = dropout_rate
+
+        
+    def _list_or_int(self, inp, pad_with_ones=False):
+        
+        # For getting kernel sizes from a single number or a length-2 list
+        if type(inp) is list:
+            output = [1, inp[0], inp[1], 1]
+        else:
+            output = [1, inp, inp, 1]
+
+        if pad_with_ones:
+            return output
+        else:
+            return [output[1], output[2]]
+        
+    def _conv2d_layer(self, x, n, kernel_size, stride, inp_depth, out_depth, 
+                      act_fn):
+        '''
+        x            : tensor
+        n            : name; string
+        kernel_size  : single int (symmetric) or list of length 2; filter sizes
+        stride       : single int (symmetric) or list of length 2; stride sizes
+        inp_depth    : int
+        out_depth    : int
+        act_fn       : e.g. tf.nn.relu
+        '''
+        
+        # Naming scope
+        with tf.variable_scope(n):
+            
+            # Kernel size
+            kernel  = self._list_or_int(kernel_size, pad_with_ones=False)
+            
+            # Weights
+            W_shape = [kernel[0], kernel[1], inp_depth, out_depth]
+            
+            W_init  = tf.random_uniform_initializer(minval=-1e-3, maxval=1e-3,
+                                                    dtype=tf.float32)
+            W       = tf.get_variable('W', W_shape, initializer=W_init)
+            
+            # Biases
+            b_init  = tf.constant_initializer(0)
+            b       = tf.get_variable('b', out_depth, initializer=b_init)
+            
+            # Strides
+            strides  = self._list_or_int(stride, pad_with_ones=True)
+            
+            # Convolve
+            pre_act = b + tf.nn.conv2d(x, W, padding='SAME', strides=strides)
+            self._histsum('pre_act', pre_act)
+            
+            # Activate
+            if act_fn is not None:
+                output = act_fn(pre_act)
+            else:
+                output = pre_act
+                
+            self._histsum('pre_pool', output)
+            
+        self._get_weights_and_bias_summaries(n)
+            
+        return output
+        
+        
+    def _pool_layer(self, x, n, pool_size, pool_stride, 
+                    pool_type=tf.nn.max_pool):
+        '''
+        x           : tensor
+        n           : name; string
+        pool_size   : int (symmetric) or list of 2 ints; size of pooling op
+        pool_stride : int (symmetric) or list of 2 ints; stride of pooling op
+        pool_type   : e.g. tf.nn.max_pool
+        '''
+        
+        # Naming scope
+        with tf.variable_scope(n):
+            
+            # Kernel size
+            kernel = self._list_or_int(pool_size, pad_with_ones=True)
+            
+            # Strides
+            stride = self._list_or_int(pool_stride, pad_with_ones=True)
+            
+            # Apply pooling
+            out = pool_type(x, ksize=kernel, strides=stride, padding='SAME')
+            self._histsum('post_pool', out)
+        
+        return out
+        
+    def _hidden_layer(self, x, n, output_shape=None, act_fn=None, 
+                      reg_strength=0., input_shape=None):
+        '''
+        x            : tensor
+        n            : name; string
+        input_shape  : int
+        output_shape : int
+        act_fn       : e.g. tf.nn.relu
+        reg_strength : float - regularisation strength
+        '''
+        
+        # Naming scope
+        with tf.variable_scope(n):
+            
+            # Infer input shape from x
+            if input_shape is not None:
+                if x.get_shape()[1] != input_shape:
+                    raise Warning('x shape in hidden layer != given shape. ' +\
+                                  'This may cause problems...')
+            else:
+                input_shape = x.get_shape()[1]
+            
+            # Weights
+            W_shape = [input_shape, output_shape]
+            W_init  = tf.random_uniform_initializer(minval=-1e-3, maxval=1e-3, 
+                                                    dtype=tf.float32)
+            W_reg   = regularizers.l2_regularizer(reg_strength)
+            W       = tf.get_variable('W', W_shape, initializer=W_init, 
+                                      regularizer=W_reg)
+            
+            # Biases
+            b_init  = tf.constant_initializer(0)
+            b       = tf.get_variable('b', output_shape, initializer=b_init)
+            
+            # Linear transform
+            pre_act = tf.matmul(x, W) + b
+            self._histsum('pre_act', pre_act)
+            
+            # Activate
+            if act_fn is not None:
+                pre_drop = act_fn(pre_act)
+            else:
+                pre_drop = pre_act
+            self._histsum('pre_drop', pre_drop)
+            
+            # Dropout
+            output = tf.cond(self.is_training,
+                             lambda: tf.nn.dropout(pre_drop, 
+                                                   1.0 - self.dropout_rate),
+                             lambda: pre_drop)
+            
+            self._histsum('post_drop', output)
+            
+        self._get_weights_and_bias_summaries(n)
+                             
+        return output
+        
+    def _histsum(self, n, x):
+        tf.histogram_summary(tf.get_variable_scope().name + '/' + n, x)
+        
+    def _histmean_summary(self, varname, scope):
+        var = tf.get_variable(varname)
+        tf.histogram_summary(scope + '/' + varname + '_hist', var)
+      
+    def _get_weights_and_bias_summaries(self, scope):
+        with tf.variable_scope(scope, reuse=True):
+            # Get variables
+            self._histmean_summary('W', scope)
+            self._histmean_summary('b', scope)
+        
+    def inference(self, x):
+        """
+        Performs inference given an input tensor. This is the central portion
+        of the network where we describe the computation graph. Here an input
+        tensor undergoes a series of convolution, pooling and nonlinear operations
+        as defined in this method. For the details of the model, please
+        see assignment file.
+
+        Here we recommend you to consider using variable and name scopes in order
+        to make your graph more intelligible for later references in TensorBoard
+        and so on. You can define a name scope for the whole model or for each
+        operator group (e.g. conv+pool+relu) individually to group them by name.
+        Variable scopes are essential components in TensorFlow for parameter sharing.
+        Although the model(s) which are within the scope of this class do not require
+        parameter sharing it is a good practice to use variable scope to encapsulate
+        model.
+
+        Args:
+          x: 4D float Tensor of size [batch_size, input_height, input_width, input_channels]
+
+        Returns:
+          logits: 2D float Tensor of size [batch_size, self.n_classes]. Returns
+                  the logits outputs (before softmax transformation) of the
+                  network. These logits can then be used with loss and accuracy
+                  to evaluate the model.
+        """
+        with tf.variable_scope('ConvNet'):
+            ########################
+            # PUT YOUR CODE HERE  #
+            ########################
+        # can probably infer inp_depth from x later...
+            self.conv1 = self._conv2d_layer(x, 'conv1',
+                                            kernel_size = 5, 
+                                            stride      = 1, 
+                                            inp_depth   = 3,
+                                            out_depth   = 64,
+                                            act_fn      = tf.nn.relu)
+            self.pool1 = self._pool_layer(self.conv1, 'pool1', pool_size=3, pool_stride=2)
+            
+            self.conv2 = self._conv2d_layer(self.pool1, 'conv2',
+                                            kernel_size = 5, 
+                                            stride      = 1, 
+                                            inp_depth   = 64,
+                                            out_depth   = 64,
+                                            act_fn      = tf.nn.relu)
+            self.pool2 = self._pool_layer(self.conv2, 'pool2', pool_size=3, pool_stride=2)
+            
+            with tf.variable_scope('flatten'):
+                self.flatten = tf.reshape(self.pool2, [-1, 64 * 64])
+                
+            self.hidd1 = self._hidden_layer(self.flatten, 'hidd1',
+                                            act_fn       = tf.nn.relu,
+                                            reg_strength = 0.001,
+                                            output_shape = 384)
+            self.hidd2 = self._hidden_layer(self.hidd1, 'hidd2',
+                                            act_fn       = tf.nn.relu,
+                                            reg_strength = 0.001,
+                                            output_shape = 192)
+            self.logits = self._hidden_layer(self.hidd2, 'logits',
+                                             act_fn       = None,
+                                             reg_strength = 0.001,
+                                             output_shape = 10)
+                
+            ########################
+            # END OF YOUR CODE    #
+            ########################
+        return self.logits
+
+    def accuracy(self, logits, labels):
+        """
+        Calculate the prediction accuracy, i.e. the average correct predictions
+        of the network.
+        As in self.loss above, you can use tf.scalar_summary to save
+        scalar summaries of accuracy for later use with the TensorBoard.
+
+        Args:
+          logits: 2D float Tensor of size [batch_size, self.n_classes].
+                       The predictions returned through self.inference.
+          labels: 2D int Tensor of size [batch_size, self.n_classes]
+                     with one-hot encoding. Ground truth labels for
+                     each observation in batch.
+
+        Returns:
+          accuracy: scalar float Tensor, the accuracy of predictions,
+                    i.e. the average correct predictions over the whole batch.
+        """
+        ########################
+        # PUT YOUR CODE HERE  #
+        ########################
+        correct_prediction = tf.equal(tf.argmax(labels, 1),
+                                      tf.argmax(logits, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.scalar_summary('accuracy', accuracy)
+        ########################
+        # END OF YOUR CODE    #
+        ########################
+
+        return accuracy
+
+    def loss(self, logits, labels):
+        """
+        Calculates the multiclass cross-entropy loss from the logits predictions and
+        the ground truth labels. The function will also add the regularization
+        loss from network weights to the total loss that is return.
+        In order to implement this function you should have a look at
+        tf.nn.softmax_cross_entropy_with_logits.
+        You can use tf.scalar_summary to save scalar summaries of
+        cross-entropy loss, regularization loss, and full loss (both summed)
+        for use with TensorBoard. This will be useful for compiling your report.
+
+        Args:
+          logits: 2D float Tensor of size [batch_size, self.n_classes].
+                       The predictions returned through self.inference.
+          labels: 2D int Tensor of size [batch_size, self.n_classes]
+                       with one-hot encoding. Ground truth labels for each
+                       observation in batch.
+
+        Returns:
+          loss: scalar float Tensor, full loss = cross_entropy + reg_loss
+        """
+        ########################
+        # PUT YOUR CODE HERE  #
+        ########################
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+                            logits, labels, name='crossentropy')  
+        loss = tf.reduce_mean(cross_entropy, name='loss')
+        tf.scalar_summary('crossentropyloss', loss)
+        ########################
+        # END OF YOUR CODE    #
+        ########################
+
+        return loss
---
+++ train_model.py
@@ -0,0 +1,382 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+
+import tensorflow as tf
+import numpy as np
+import cifar10_utils
+from convnet import ConvNet
+from sklearn.manifold import TSNE
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+LEARNING_RATE_DEFAULT = 1e-4
+BATCH_SIZE_DEFAULT = 128
+MAX_STEPS_DEFAULT = 15000
+EVAL_FREQ_DEFAULT = 1000
+CHECKPOINT_FREQ_DEFAULT = 5000
+PRINT_FREQ_DEFAULT = 10
+OPTIMIZER_DEFAULT = 'ADAM'
+
+DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
+#LOG_DIR_DEFAULT = './logs/cifar10'
+LOG_DIR_DEFAULT = './logs/test'
+CHECKPOINT_DIR_DEFAULT = './checkpoints'
+
+def train_step(loss):
+    """
+    Defines the ops to conduct an optimization step. You can set a learning
+    rate scheduler or pick your favorite optimizer here. This set of operations
+    should be applicable to both ConvNet() and Siamese() objects.
+
+    Args:
+        loss: scalar float Tensor, full loss = cross_entropy + reg_loss
+
+    Returns:
+        train_op: Ops for optimization.
+    """
+    ########################
+    # PUT YOUR CODE HERE  #
+    ########################
+    raise NotImplementedError
+    with tf.name_scope('train'):
+      optimizer = tf.train.AdamOptimizer()
+      train_op = optimizer.minimize(loss)
+    ########################
+    # END OF YOUR CODE    #
+    ########################
+
+    return train_op
+
+def train():
+    """
+    Performs training and evaluation of ConvNet model.
+    
+    First define your graph using class ConvNet and its methods. Then define
+    necessary operations such as trainer (train_step in this case), savers
+    and summarizers. Finally, initialize your model within a tf.Session and
+    do the training.
+    
+    ---------------------------
+    How to evaluate your model:
+    ---------------------------
+    Evaluation on test set should be conducted over full batch, i.e. 10k images,
+    while it is alright to do it over minibatch for train set.
+    
+    ---------------------------------
+    How often to evaluate your model:
+    ---------------------------------
+    - on training set every print_freq iterations
+    - on test set every eval_freq iterations
+    
+    ------------------------
+    Additional requirements:
+    ------------------------
+    Also you are supposed to take snapshots of your model state (i.e. graph,
+    weights and etc.) every checkpoint_freq iterations. For this, you should
+    study TensorFlow's tf.train.Saver class. For more information, please
+    checkout:
+    [https://www.tensorflow.org/versions/r0.11/how_tos/variables/index.html]
+    """
+    #%%
+    # Set the random seeds for reproducibility. DO NOT CHANGE.
+    tf.set_random_seed(42)
+    np.random.seed(42)
+    
+    ########################
+    # PUT YOUR CODE HERE  #
+    ########################
+    tf.reset_default_graph()
+    
+    sess = tf.Session()
+    
+    cifar10 = cifar10_utils.get_cifar10('cifar10/cifar-10-batches-py')
+    
+    image_shape = cifar10.train.images.shape[1:4]
+    num_classes = cifar10.test.labels.shape[1]
+    
+    x = tf.placeholder(tf.float32, shape=[None] + list(image_shape), name='x')
+    y = tf.placeholder(tf.int32, shape=(None, num_classes), name='y')
+    is_training = tf.placeholder(dtype=tf.bool, shape=(), name='isTraining')
+    
+    model = ConvNet(is_training=is_training, dropout_rate=0.)
+    
+    logits = model.inference(x)
+    
+    loss = model.loss(logits, y) + \
+           sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+    tf.scalar_summary('loss_incl_reg', loss)
+    accuracy = model.accuracy(logits, y)
+    
+    with tf.name_scope('train'):
+        optimizer = tf.train.AdamOptimizer()
+        train_op = optimizer.minimize(loss)
+    
+    # Merge all the summaries
+    merged = tf.merge_all_summaries()
+    train_writer = tf.train.SummaryWriter(FLAGS.log_dir + '/train', sess.graph)
+    test_writer = tf.train.SummaryWriter(FLAGS.log_dir + '/test')
+    
+    TEST_SIZE = 1000
+    
+    # Initialise all variables
+    tf.initialize_all_variables().run(session=sess)
+    
+    # Function for getting feed dicts
+    def get_fd(c, train=True):
+        if train:
+            xd, yd = c.train.next_batch(FLAGS.batch_size)
+            return {x : xd, y : yd, is_training : True}
+        else:
+            xd, yd = c.test.images[:TEST_SIZE], c.test.labels[:TEST_SIZE]
+            return {x : xd, y : yd, is_training : False}
+    
+    saver = tf.train.Saver()
+    for epoch in range(0, FLAGS.max_steps):
+        if epoch % 100 == 0:
+            
+          # Print accuracy and loss on test set
+          summary, acc, loss_val = \
+              sess.run([merged, accuracy, loss], get_fd(cifar10, False))
+          test_writer.add_summary(summary, epoch)
+          
+          print ('\nEpoch', epoch, 
+                 '\nTest accuracy:', acc, 
+                 '\nTest loss    :', loss_val)
+          
+          # Save model checkpoint
+          if epoch > 0:
+              save_path = saver.save(sess, 
+                         'checkpoints/conv_basic_epoch' + str(epoch) + '.ckpt')
+              print("Model saved in file: %s" % save_path)
+    
+        # Do training update
+        summary, _ = sess.run([merged, train_op], 
+                              feed_dict=get_fd(cifar10, True))
+        train_writer.add_summary(summary, epoch)
+    
+    # Print the final accuracy
+    summary, acc, loss_val = \
+        sess.run([merged, accuracy, loss], get_fd(cifar10, False))
+    test_writer.add_summary(summary, epoch + 1)
+    print ('\nFinal test accuracy:', acc, '\nFinal test loss    :', loss_val)
+    #%%
+    ########################
+    # END OF YOUR CODE    #
+    ########################
+
+
+def train_siamese():
+    """
+    Performs training and evaluation of Siamese model.
+
+    First define your graph using class Siamese and its methods. Then define
+    necessary operations such as trainer (train_step in this case), savers
+    and summarizers. Finally, initialize your model within a tf.Session and
+    do the training.
+
+    ---------------------------
+    How to evaluate your model:
+    ---------------------------
+    On train set, it is fine to monitor loss over minibatches. On the other
+    hand, in order to evaluate on test set you will need to create a fixed
+    validation set using the data sampling function you implement for siamese
+    architecture. What you need to do is to iterate over all minibatches in
+    the validation set and calculate the average loss over all minibatches.
+
+    ---------------------------------
+    How often to evaluate your model:
+    ---------------------------------
+    - on training set every print_freq iterations
+    - on test set every eval_freq iterations
+
+    ------------------------
+    Additional requirements:
+    ------------------------
+    Also you are supposed to take snapshots of your model state (i.e. graph,
+    weights and etc.) every checkpoint_freq iterations. For this, you should
+    study TensorFlow's tf.train.Saver class. For more information, please
+    checkout:
+    [https://www.tensorflow.org/versions/r0.11/how_tos/variables/index.html]
+    """
+
+    # Set the random seeds for reproducibility. DO NOT CHANGE.
+    tf.set_random_seed(42)
+    np.random.seed(42)
+
+    ########################
+    # PUT YOUR CODE HERE  #
+    ########################
+    raise NotImplementedError
+    ########################
+    # END OF YOUR CODE    #
+    ########################
+
+
+#def feature_extraction():
+"""
+This method restores a TensorFlow checkpoint file (.ckpt) and rebuilds inference
+model with restored parameters. From then on you can basically use that model in
+any way you want, for instance, feature extraction, finetuning or as a submodule
+of a larger architecture. However, this method should extract features from a
+specified layer and store them in data files such as '.h5', '.npy'/'.npz'
+depending on your preference. You will use those files later in the assignment.
+
+Args:
+    [optional]
+Returns:
+    None
+"""
+
+########################
+# PUT YOUR CODE HERE  #
+########################
+tf.reset_default_graph()
+
+sess = tf.Session()
+
+cifar10 = cifar10_utils.get_cifar10('cifar10/cifar-10-batches-py')
+
+image_shape = cifar10.train.images.shape[1:4]
+num_classes = cifar10.test.labels.shape[1]
+
+x = tf.placeholder(tf.float32, shape=[None] + list(image_shape), name='x')
+y = tf.placeholder(tf.int32, shape=(None, num_classes), name='y')
+is_training = tf.placeholder(dtype=tf.bool, shape=(), name='isTraining')
+
+model = ConvNet(is_training=is_training, dropout_rate=0.)
+
+logits = model.inference(x)
+
+loss = model.loss(logits, y) + \
+       sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+tf.scalar_summary('loss_incl_reg', loss)
+accuracy = model.accuracy(logits, y)
+
+with tf.name_scope('train'):
+    optimizer = tf.train.AdamOptimizer()
+    train_op = optimizer.minimize(loss)
+
+# Merge all the summaries
+merged = tf.merge_all_summaries()
+train_writer = tf.train.SummaryWriter(FLAGS.log_dir + '/train', sess.graph)
+test_writer = tf.train.SummaryWriter(FLAGS.log_dir + '/test')
+
+TEST_SIZE = 1000
+
+# Initialise all variables
+tf.initialize_all_variables().run(session=sess)
+
+# Function for getting feed dicts
+def get_fd(c, train=True):
+    if train:
+        xd, yd = c.train.next_batch(FLAGS.batch_size)
+        return {x : xd, y : yd, is_training : True}
+    else:
+        xd, yd = c.test.images[:TEST_SIZE], c.test.labels[:TEST_SIZE]
+        return {x : xd, y : yd, is_training : False}
+
+saver = tf.train.Saver()
+#%%
+checkpoint_path = '/home/liam/cloud/y2uni/dl/ass3/checkpoints/'
+checkpoint_file = 'conv_basic_epoch600.ckpt'
+saver.restore(sess, checkpoint_path + checkpoint_file)
+
+# Get the features for the second fully-connected layer
+x_data, y_data = \
+    cifar10.test.images[:TEST_SIZE], cifar10.test.labels[:TEST_SIZE]
+features = sess.run([model.hidd2], 
+                    {x : x_data, y : y_data, is_training : False})[0]
+
+# Get t-SNE manifold of these features
+tsne = TSNE()
+manifold = tsne.fit_transform(features)
+
+def colourful_scatter(x_2d, one_hot, num_classes):
+# Make a 2D scatter plot with one colour per class
+    colors = cm.rainbow(np.linspace(0, 1, num_classes))
+    for i in range(num_classes):
+        subset = np.where(one_hot.argmax(1) == i)[0]
+        plt.scatter(x_2d[subset, 0], x_2d[subset, 1], color=colors[i])
+    plt.show()
+
+# Plot the manifold
+colourful_scatter(manifold, y_data, num_classes)
+
+#%%
+
+#%%
+########################
+# END OF YOUR CODE    #
+########################
+
+def initialize_folders():
+    """
+    Initializes all folders in FLAGS variable.
+    """
+
+    if not tf.gfile.Exists(FLAGS.log_dir):
+        tf.gfile.MakeDirs(FLAGS.log_dir)
+
+    if not tf.gfile.Exists(FLAGS.data_dir):
+        tf.gfile.MakeDirs(FLAGS.data_dir)
+
+    if not tf.gfile.Exists(FLAGS.checkpoint_dir):
+        tf.gfile.MakeDirs(FLAGS.checkpoint_dir)
+
+def print_flags():
+    """
+    Prints all entries in FLAGS variable.
+    """
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
+
+def main(_):
+    print_flags()
+
+    initialize_folders()
+
+    if False:#FLAGS.is_train:
+        if FLAGS.train_model == 'linear':
+            train()
+        elif FLAGS.train_model == 'siamese':
+            train_siamese()
+        else:
+            raise ValueError("--train_model argument can be linear or siamese")
+    else:
+        feature_extraction()
+
+if __name__ == '__main__':
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--learning_rate', type = float, default = LEARNING_RATE_DEFAULT,
+                      help='Learning rate')
+    parser.add_argument('--max_steps', type = int, default = MAX_STEPS_DEFAULT,
+                      help='Number of steps to run trainer.')
+    parser.add_argument('--batch_size', type = int, default = BATCH_SIZE_DEFAULT,
+                      help='Batch size to run trainer.')
+    parser.add_argument('--print_freq', type = int, default = PRINT_FREQ_DEFAULT,
+                      help='Frequency of evaluation on the train set')
+    parser.add_argument('--eval_freq', type = int, default = EVAL_FREQ_DEFAULT,
+                      help='Frequency of evaluation on the test set')
+    parser.add_argument('--checkpoint_freq', type = int, default = CHECKPOINT_FREQ_DEFAULT,
+                      help='Frequency with which the model state is saved.')
+    parser.add_argument('--data_dir', type = str, default = DATA_DIR_DEFAULT,
+                      help='Directory for storing input data')
+    parser.add_argument('--log_dir', type = str, default = LOG_DIR_DEFAULT,
+                      help='Summaries log directory')
+    parser.add_argument('--checkpoint_dir', type = str, default = CHECKPOINT_DIR_DEFAULT,
+                      help='Checkpoint directory')
+    parser.add_argument('--is_train', type = str, default = True,
+                      help='Training or feature extraction')
+    parser.add_argument('--train_model', type = str, default = 'linear',
+                      help='Type of model. Possible options: linear and siamese')
+
+    FLAGS, unparsed = parser.parse_known_args()
+
+    tf.app.run()
